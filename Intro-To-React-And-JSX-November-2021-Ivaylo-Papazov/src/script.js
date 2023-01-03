7800dc7a021d454e543a6700124dbfe9a8414998
Simeon Yordanov <lookmanche@gmail.com>
2023-01-03 18:02:24 +0200

    created script.js copy for jsx syntax and moved html and css file in build folder

--- Intro-To-React-And-JSX-November-2021-Ivaylo-Papazov/src/script.js
+++ Intro-To-React-And-JSX-November-2021-Ivaylo-Papazov/src/script.js
@@ -1,10 +1,16 @@
 let rootElement = document.getElementById('root');
 
 //Traditional way of createing a React Element:
-let reactElement = React.createElement(
+let tradinitionalReactElement = React.createElement(
     'header', 
     { className: 'site-header'}, 
     React.createElement('h1', { id: 'main-heading' }, 'hello React'),
     React.createElement('h1', { id: 'main-heading' }, 'hello React'),);
 
-ReactDOM.render(reactElement, rootElement);
+let jsxReactElement = <header>
+    <h1>Hello from JSX</h1>
+    <h2>The best framework in the world!</h2>
+</header>;
+    
+ReactDOM.render(tradinitionalReactElement, rootElement);
+ReactDOM.render(jsxReactElement, rootElement);
